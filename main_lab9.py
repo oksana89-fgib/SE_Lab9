431c41840e0768c91e66aaae5af9559a1b506d35
Oksana kireeva <oksana.k.s.89@yandex.ru>
2024-05-31 08:33:45 +0300

    init commit

--- main_lab9.py
+++ main_lab9.py
@@ -1,18 +1,12 @@
-import matplotlib.pyplot as plt
 import streamlit as st
+import matplotlib.pyplot as plt
 
-st.image('photo1715528350.jpeg')
-st.header('Информация по пассажирам Титаника')
-st.write('Количество выживших мужчин по каждому классу обслуживания в заданном возростном диапазоне.')
-with open("data.csv") as file:
-    Passenclass1 = {
-        'selected': 0
-    }
-    Passenclass2 = {
-        'selected': 0
-    }
-    Passenclass3 = {
-        'selected': 0
+
+def man_survived(file):
+    survived_passengers = {
+        'class_1': {},
+        'class_2': {},
+        'class_3': {},
     }
     for line in file:
         data = line.split(',')
@@ -23,51 +17,56 @@ with open("data.csv") as file:
         elif data[2] == '1':
             age = data[6]
             if age == '':
-                age = 0
-            if not age in Passenclass1:
-                Passenclass1[age] = 1
+                age = '0'
+            if age not in survived_passengers['class_1']:
+                survived_passengers['class_1'][age] = 1
             else:
-                Passenclass1[age] += 1
+                survived_passengers['class_1'][age] += 1
         elif data[2] == '2':
             age = data[6]
             if age == '':
-                age = 0
-            if not age in Passenclass2:
-                Passenclass2[age] = 1
+                age = '0'
+            if age not in survived_passengers['class_2']:
+                survived_passengers['class_2'][age] = 1
             else:
-                Passenclass2[age] += 1
+                survived_passengers['class_2'][age] += 1
         elif data[2] == '3':
             age = data[6]
             if age == '':
-                age = 0
-            if not age in Passenclass3:
-                Passenclass3[age] = 1
+                age = '0'
+            if age not in survived_passengers['class_3']:
+                survived_passengers['class_3'][age] = 1
             else:
-                Passenclass3[age] += 1
-    selected = st.slider("Возраст пассажиров", 0, 100, (0, 100))
-    for ages in Passenclass1:
-        if ages == 'selected' or ages == '':
-            continue
-        if selected[0] <= float(ages) <= selected[1]:
-            Passenclass1['selected'] += Passenclass1[ages]
-    for ages in Passenclass2:
-        if ages == 'selected' or ages == '':
-            continue
-        if selected[0] <= float(ages) <= selected[1]:
-            Passenclass2['selected'] += Passenclass2[ages]
-    for ages in Passenclass3:
-        if ages == 'selected' or ages == '':
-            continue
-        if selected[0] <= float(ages) <= selected[1]:
-            Passenclass3['selected'] += Passenclass3[ages]
-clas = ['Класс 1', 'Класс 2', 'Класс 3']
-Passenger = [Passenclass1['selected'], Passenclass2['selected'], Passenclass3['selected']]
-fig = plt.figure(figsize=(10, 8))
-plt.bar(clas, Passenger)
-for i in range(len(clas)):
-    plt.text(i, Passenger[i], Passenger[i], ha='center')
-plt.xlabel('Класс обслуживания')
-plt.ylabel('Число выживших')
-plt.title('Диаграмма - количество выживших мужчин')
+                survived_passengers['class_3'][age] += 1
+    return survived_passengers
 
-st.pyplot(fig)
+
+def select_ages(passengers, selected_ages):
+    selected_passengers = {
+        'class_1': 0,
+        'class_2': 0,
+        'class_3': 0,
+    }
+    for n_class in passengers:
+        for age in passengers[n_class]:
+            if selected_ages[0] <= float(age) <= selected_ages[1]:
+                selected_passengers[n_class] += passengers[n_class][age]
+    return [selected_passengers['class_1'], selected_passengers['class_2'], selected_passengers['class_3']]
+
+def SE_lab9():
+    st.image('photo1715528350.jpeg')
+    st.header('Информация по пассажирам Титаника')
+    st.write('Количество выживших мужчин по каждому классу обслуживания в заданном возростном диапазоне.')
+    with open('data.csv') as file:
+        mans_survived = man_survived(file)
+    clas = ['Класс 1', 'Класс 2', 'Класс 3']
+    selected = st.slider("Возраст пассажиров", 0, 100, (0, 100))
+    passenger = select_ages(mans_survived, selected)
+    fig = plt.figure(figsize=(10, 8))
+    plt.bar(clas, passenger)
+    for i in range(len(clas)):
+        plt.text(i, passenger[i], passenger[i], ha='center')
+    plt.xlabel('Класс обслуживания')
+    plt.ylabel('Число выживших')
+    plt.title('Диаграмма - количество выживших мужчин')
+    st.pyplot(fig)
